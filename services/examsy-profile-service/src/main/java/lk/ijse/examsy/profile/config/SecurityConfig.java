04808ce69267bea5fc71e936c665bbc32e2adee3
Ruvinda-Shaluka <shalukaofficial24@gmail.com>
2026-09-04 00:24:21 +0530

    refactor(profile): update SecurityConfig and REST controllers to use examsy-common imports

--- services/examsy-profile-service/src/main/java/lk/ijse/examsy/profile/config/SecurityConfig.java
+++ services/examsy-profile-service/src/main/java/lk/ijse/examsy/profile/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package lk.ijse.examsy.profile.config;
 
-import lk.ijse.examsy.profile.security.JwtAuthFilter;
+import lk.ijse.examsy.common.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
